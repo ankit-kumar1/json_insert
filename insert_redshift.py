ab78cc2967f22bd1fbe49729b78e61270542e394
Ankit Kumar <35533136+ankit-kumar1@users.noreply.github.com>
2019-03-18 00:47:57 -0400

    Create insert_redshift.py

---
+++ insert_redshift.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sqlalchemy import create_engine, types
+
+##create database connectiong
+conn = create_engine('postgresql://username:password@localhost:5439/production')
+
+##write json file to dataframes
+df = pd.read_json('/pathtofile/message_blast.json',
+                  orient='columns', lines=True)
+#print(df)
+
+##insert to database
+df.to_sql('message_blast', conn, index=False, if_exists='append',
+          schema='analytics',
+          dtype=
+          {'opens': types.JSON,
+           'clicks': types.JSON}
+          )
+
+
